f21a8e69280bcae04286a9a6c530822efcd6b635
Agustinadearagon <72619859+Agustinadearagon@users.noreply.github.com>
2026-07-06 18:30:31 +0200

    Update cache version to 'quien-es-quien-v2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'quien-es-quien-v1';
+const CACHE = 'quien-es-quien-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
